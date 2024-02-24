3dc7fe7a0ad0767b626ce8428d89e3b921fe5734
Azraakbb <azraakbaba145@gmail.com>
2024-02-24 22:27:37 +0300

    ilk form uygulamam

---
+++ Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace c_ilkodev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.listekutusu = new System.Windows.Forms.ListView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.analizsonuc = new System.Windows.Forms.TextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.karakterkutusu = new System.Windows.Forms.CheckBox();
+            this.sayikutusu = new System.Windows.Forms.CheckBox();
+            this.metinkutusu = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.bildirims = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabPage5.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.LightSlateGray;
+            this.tabPage5.Controls.Add(this.listekutusu);
+            this.tabPage5.Controls.Add(this.button6);
+            this.tabPage5.Controls.Add(this.button7);
+            this.tabPage5.Controls.Add(this.button8);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(792, 421);
+            this.tabPage5.TabIndex = 2;
+            this.tabPage5.Text = "Loglama";
+            this.tabPage5.Click += new System.EventHandler(this.tabPage5_Click);
+            // 
+            // listekutusu
+            // 
+            this.listekutusu.HideSelection = false;
+            this.listekutusu.Location = new System.Drawing.Point(126, 15);
+            this.listekutusu.Name = "listekutusu";
+            this.listekutusu.Size = new System.Drawing.Size(539, 313);
+            this.listekutusu.TabIndex = 8;
+            this.listekutusu.UseCompatibleStateImageBehavior = false;
+            this.listekutusu.View = System.Windows.Forms.View.List;
+            this.listekutusu.SelectedIndexChanged += new System.EventHandler(this.listekutusu_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(488, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(177, 40);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Temizle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(312, 352);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(170, 40);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "< Geri ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(126, 352);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(180, 40);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Ana ekran";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Thistle;
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.analizsonuc);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Analiz sonuçları";
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(481, 346);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 40);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "İleri";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(305, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 40);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(119, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 40);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Geri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // analizsonuc
+            // 
+            this.analizsonuc.Location = new System.Drawing.Point(95, 22);
+            this.analizsonuc.Multiline = true;
+            this.analizsonuc.Name = "analizsonuc";
+            this.analizsonuc.Size = new System.Drawing.Size(588, 296);
+            this.analizsonuc.TabIndex = 0;
+            this.analizsonuc.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.RosyBrown;
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.karakterkutusu);
+            this.tabPage1.Controls.Add(this.sayikutusu);
+            this.tabPage1.Controls.Add(this.metinkutusu);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ana Ekran";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(375, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "İleri >";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(375, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Çalıştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // karakterkutusu
+            // 
+            this.karakterkutusu.AutoSize = true;
+            this.karakterkutusu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.karakterkutusu.Location = new System.Drawing.Point(482, 218);
+            this.karakterkutusu.Name = "karakterkutusu";
+            this.karakterkutusu.Size = new System.Drawing.Size(86, 22);
+            this.karakterkutusu.TabIndex = 6;
+            this.karakterkutusu.Text = "Karakter";
+            this.karakterkutusu.UseVisualStyleBackColor = true;
+            this.karakterkutusu.CheckedChanged += new System.EventHandler(this.karakterkutusu_CheckedChanged);
+            // 
+            // sayikutusu
+            // 
+            this.sayikutusu.AutoSize = true;
+            this.sayikutusu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayikutusu.Location = new System.Drawing.Point(392, 218);
+            this.sayikutusu.Name = "sayikutusu";
+            this.sayikutusu.Size = new System.Drawing.Size(58, 22);
+            this.sayikutusu.TabIndex = 5;
+            this.sayikutusu.Text = "Sayı";
+            this.sayikutusu.UseVisualStyleBackColor = true;
+            // 
+            // metinkutusu
+            // 
+            this.metinkutusu.AutoSize = true;
+            this.metinkutusu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.metinkutusu.Location = new System.Drawing.Point(294, 218);
+            this.metinkutusu.Name = "metinkutusu";
+            this.metinkutusu.Size = new System.Drawing.Size(66, 22);
+            this.metinkutusu.TabIndex = 4;
+            this.metinkutusu.Text = "Metin";
+            this.metinkutusu.UseVisualStyleBackColor = true;
+            this.metinkutusu.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(168, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Aranacak ifade:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(168, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Metni giriniz:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(294, 169);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(344, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(294, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 99);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // bildirims
+            // 
+            this.bildirims.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.bildirims.Icon = ((System.Drawing.Icon)(resources.GetObject("bildirims.Icon")));
+            this.bildirims.Text = "notifyIcon1";
+            this.bildirims.Visible = true;
+            this.bildirims.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox karakterkutusu;
+        private System.Windows.Forms.CheckBox sayikutusu;
+        private System.Windows.Forms.CheckBox metinkutusu;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox analizsonuc;
+        private System.Windows.Forms.ListView listekutusu;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NotifyIcon bildirims;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_ilkodev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bildirims.BalloonTipText = "Nesne Yönelimli Programlama Ödev 1" ;
+            bildirims.BalloonTipTitle = "Programa Hoş geldiniz!"; //bildirim balonunun başlık ve metinini belirledik
+            bildirims.ShowBalloonTip(4000);
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string paragraf = textBox1.Text;
+            string arananifade = textBox2.Text;
+
+            if (string.IsNullOrEmpty(paragraf) || string.IsNullOrEmpty(arananifade)) //aranan ifade kutusu boş mu kontrol eder
+            {
+                MessageBox.Show("Lütfen metin ve aranacak ifadeyi girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if(!kontrol(arananifade,metinkutusu.Checked,sayikutusu.Checked,karakterkutusu.Checked))
+            {
+                return; 
+            }
+
+            int arananifadeninuzunluk = arananifade.Length;
+            int girilenmetinuzunluk = textBox1.Text.Length;
+            int ilkgecen = paragraf.IndexOf(arananifade); //IndexOf ile paragrafta ilk geçtiği yer bulunur
+
+            analizsonuc.Text = "Girilen metnin uzunluğu:" + girilenmetinuzunluk + Environment.NewLine;
+            analizsonuc.Text += "Aranan metnin uzunluğu:" + arananifadeninuzunluk + Environment.NewLine;
+
+            if (ilkgecen != -1)
+            {
+                analizsonuc.Text += "Aranan ifade metinde bulunuyor. Geçtiği ilk indeks: " + ilkgecen + Environment.NewLine;
+
+            }
+            else
+            {
+                analizsonuc.Text = "Aranan ifade bulunamadı"; // eşitlememizin sebebi metinde bulunmuyorsa bilgileri ekrana yazmaz
+            }
+
+            int sayac = 0;
+            int index = 0;
+
+            while ((index = paragraf.IndexOf(arananifade, index + 1)) != -1)
+            {
+                sayac++;
+            }
+            analizsonuc.Text += "Metinde kaç adet bulunuyor:";
+            analizsonuc.Text += sayac.ToString(); //string değerine çevirir
+
+            string analizsonucuekle = $"{listekutusu.Items.Count +1}. Metinde '{arananifade}' ifadesi aranmış ve {sayac} defa bulunmuştur. Metnin uzunluğu {girilenmetinuzunluk}'dır.";
+
+            listekutusu.Items.Add(analizsonucuekle); //listekutusuna analiz sonuçlarını ekledik Add ile
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            analizsonuc.Clear(); //kutuyu temizler
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 0;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage5; //sayfalar arası geçişi sağlayan komut
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listekutusu.Clear();
+        }
+
+        private void listekutusu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+        private bool kontrol(string arananifade,bool metin,bool sayi,bool karakter)
+        {
+            bool sayivar = false;
+            bool karakterVar = false;
+            bool metinvar = false;
+
+
+            if (sayi)
+            {
+                foreach (char karktr in arananifade)
+                {
+                    if (char.IsNumber(karktr))
+                    {
+                        sayivar = true;
+                        break;
+                    }
+                }
+                if (!sayivar) //false ise anlamına gelir
+                {
+                    MessageBox.Show("Aranan ifade sayı içermemektedir ");
+                    return false;
+                }
+            }
+
+
+            if (karakter)
+            {
+                foreach (char sembol in arananifade)
+                {
+                    if (!char.IsLetterOrDigit(sembol)) // her ikisi de değilse karakter olarak kabul eder
+                    {
+                        karakterVar = true;
+                    }
+                }
+                if (!karakterVar)
+                {
+                    MessageBox.Show("Aranan ifade karakter değil.");
+                    return false;
+                }
+
+
+            }
+
+
+            if (metin)
+            {
+                foreach (char metinn in arananifade)
+                {
+                    if (char.IsLetter(metinn))
+                    {
+                        metinvar=true;
+                    }
+                }
+                if (!metinvar)
+                {
+                    MessageBox.Show("Aranan ifade metin içermemektedir.");
+                    return false;
+
+                }
+
+            }
+            return true;
+
+        }
+
+        private void karakterkutusu_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
